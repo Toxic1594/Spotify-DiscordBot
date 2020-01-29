774bdb6a13bd57b30f27a647d6c177903c3d1cfb
Toxic1594 <52114232+Toxic1594@users.noreply.github.com>
2020-01-29 07:39:48 +0100

    upload

---
+++ DiscordBot-1/Modules/Commands.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using System.IO;
+namespace DiscordBot_1.Modules
+{
+   
+    class Commands : ModuleBase<SocketCommandContext>
+    {
+        Program prg = new Program();
+        [Command("account")]
+        public async Task Account()
+        {
+            
+            var user = Context.User;
+            if (File.Exists("/stuff/users/" + user + ".txt"))
+            {
+
+            }
+            else
+            {
+                var myFile = File.Create("/stuff/users/" + user + ".txt");
+                myFile.Close();
+            }
+
+
+            //read time
+            StreamReader strmread = new StreamReader("/stuff/users/" + user + ".txt");
+            string test = strmread.ReadToEnd();
+            strmread.Close();
+            int timeleft = 0;
+            if (test != "")
+            {
+                TimeSpan timespan = DateTime.Parse(test) - DateTime.Now;
+                timeleft = (int)timespan.TotalMinutes;
+            }
+            if (test == "" || timeleft <= 0)
+            {
+                
+                string account = readAccountList("");
+                if (account == "")
+                {
+                    await ReplyAsync("Accounts out of Stock");
+                }
+                else
+                {
+                    StreamWriter strmwriter = new StreamWriter("/stuff/users/" + user + ".txt");
+                    strmwriter.WriteLine(DateTime.Now.AddDays(1));
+                    strmwriter.Close();
+                    await ReplyAsync("Check your PM´s");
+                    await Discord.UserExtensions.SendMessageAsync(user, "Thanks for using Singleplayer´s Bot, Here is your Account-> " +account);
+                }
+
+
+            }
+            else
+            {
+                try
+                {
+                    await ReplyAsync("Time until u can request again: " + timeleft + "Minutes");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: " + ex);
+                    await ReplyAsync("Error , check console");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+
+
+
+
+
+        }
+
+
+        public string readAccountList(string returnaccount)
+        {
+            if (Program.TESTLOL.Count <= 0) return "";
+                returnaccount = Program.TESTLOL[0];
+                Program.TESTLOL.RemoveAt(0);
+
+                return returnaccount;
+            
+
+            
+        }
+
+    }
+}
---
+++ DiscordBot-1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using DiscordBot_1.Modules;
+using System.IO;
+namespace DiscordBot_1
+{
+    class Program
+    {
+        public static List<string> TESTLOL;
+        public static void Main(string[] args)
+        {
+            Program ppp = new Program();
+            ppp.additemstolist();
+           
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        private Task BotLoggedIn()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss") + " Successfully logged in!");
+            Console.ForegroundColor = ConsoleColor.White;
+            return Task.CompletedTask;
+        }
+
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        public async Task MainAsync()
+        {
+
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                    .AddSingleton(_client)
+                    .AddSingleton(_commands)
+                    .BuildServiceProvider();
+            _client.Log += Log;
+            _client.LoggedIn += BotLoggedIn;
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = "NjAwNjk1MTUxNTc2MjE5NjU4.XibqkA.3I-68BOViZWOL_SKKTexzBlMEuM";
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await _client.SetStatusAsync(UserStatus.Online);
+            await _client.SetGameAsync("Dropping Spotify Accs for you <3");                       
+                // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModuleAsync(typeof(Modules.Commands), _services);
+        }
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message == null) return;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+            if (context.Channel.Id != 663538221539655680) return;
+            int argPos = 0;
+            //if (message.Author.Username != "Singleplayer#1594") return;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+
+        public void additemstolist()
+        {
+            TESTLOL = File.ReadLines("/stuff/Spotify/Premium.txt").ToList();
+            for (int i =0; i<TESTLOL.Count; i++)
+            {
+                Console.WriteLine(TESTLOL[i]);
+            }
+            
+        }
+    }
+}
